8f47dbfd0e6d8c1a72011f1987106522f7266a77
diegodi87 <diegodi87@gmail.com>
2022-02-02 17:30:49 -0300

    first commit

---
+++ meulink/src/pages/home/index.js
@@ -0,0 +1,27 @@
+import { FiLink } from 'react-icons/fi';
+
+
+export default function Home() {
+  return (
+    <div className="containter-home" >
+      <div className="logo">
+        <img src="/logo.png" alt="Sujeito Link Logo" />
+        <h1> Registrodelink</h1>
+        <span>Cole seu link aqui para encurta-lo.</span>
+      </div>
+
+      <div className="area-input">
+        <div>
+          <FiLink size={24} color="#FFF" />
+          <input
+            placeholder="Cole seu link aqui"
+          />
+        </div>
+
+        <button> Encurtar link</button>
+
+      </div>
+
+    </div>
+  )
+}
