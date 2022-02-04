3ef48ef3b76527ea9dade48e6ce92ccb64964d4b
diegodi87 <diegodi87@gmail.com>
2022-02-04 17:00:51 -0300

    csslink]

--- meulink/src/pages/links/index.js
+++ meulink/src/pages/links/index.js
@@ -1,9 +1,25 @@
-
+import { icons } from 'react-icons'
+import './links.css'
+import { FiArrowLeft, Filink, FiTrash } from 'react-icons/fi'
 
 export default function Links(){
     return(
-      <div>
-        <h1> Página de links</h1>
+      <div className='"link-container'>
+        <div className='links-header'>
+          <FiArrowLeft size={38} color="#FFF" />
+          <h1> Meus Links</h1>
+        </div>
+
+        <div className='Links-item'>
+          <button className="link">
+            <Filink size={18} color="#FFF" />
+            httpS://sujeitoprogramador.com
+          </button>
+          <button>
+            <FiTrash size={24} color="#FF5454"/>
+          </button>
+        </div>
+
       </div>
     )
   }
